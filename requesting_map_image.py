eb8ad6f0e46c62e7a5860a66ec05d08ed7e591a6
vasilmao <vasezs@inbox.ru>
2018-03-07 18:21:09 +0400

    updated 3

--- requesting_map_image.py
+++
@@ -1,9 +0,0 @@
-def map_request(centerx, centery, sp1, sp2):
-    import requests, sys
-    map_request = "http://static-maps.yandex.ru/1.x/?l=sat&ll={},{}&spn={},{}&l=map".format(str(centerx), str(centery), str(sp1), str(sp2))
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-    return response.content
